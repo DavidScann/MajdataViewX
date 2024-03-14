b9c5f3a910d8335b15e4476f90f735c780ea2071
LeZi <lezi@leziblog.com>
2024-03-14 13:28:24 +0800

    Fix：在Note附近开始播放，Note的TapLine丢失问题
    Signed-off-by: re-poem <BUCYUM@gmail.com>

--- Assets/Scripts/Notes/HoldDrop.cs
+++ Assets/Scripts/Notes/HoldDrop.cs
@@ -175,6 +175,7 @@ public class HoldDrop : NoteLongDrop
                 distance = 4.8f;
                 //holdEffect.SetActive(true);
                 PlayHoldEffect();
+                startHoldShine();
             }
             else if (holdDistance < 1.225f && distance < 4.8f) // 头未到达 尾未出现
             {
@@ -185,6 +186,7 @@ public class HoldDrop : NoteLongDrop
                 distance = 4.8f;
                 //holdEffect.SetActive(true);
                 PlayHoldEffect();
+                startHoldShine();
 
                 holdEndRender.enabled = true;
             }
@@ -212,11 +214,7 @@ public class HoldDrop : NoteLongDrop
     {
         var endTime = time + LastFor;
         GameObject.Find("NoteEffects").GetComponent<NoteEffectManager>().ResetEffect(startPosition);
-        holdEffect.SetActive(true);
-
-        if (LastFor <= 0.3)
-            return;
-        else if (!holdAnimStart && timeProvider.AudioTime - time > 0.1)//忽略开头6帧与结尾12帧
+        if (!holdAnimStart && timeProvider.AudioTime - time > 0.1)
         {            
             holdAnimStart = true;
             animator.runtimeAnimatorController = HoldShine;
@@ -230,6 +228,11 @@ public class HoldDrop : NoteLongDrop
                 sprRenderer.sprite = holdOnSpr;
 
         }
+    }
+    void PlayHoldEffect()
+    {
+        if (timeProvider.AudioTime - time > 0.1) holdEffect.SetActive(true);
+        else holdEffect.SetActive(false); 
     }
 
     private Vector3 getPositionFromDistance(float distance)
