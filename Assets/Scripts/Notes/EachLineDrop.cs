b0a45c506c0b00cb7327dd92003ae9505d38fe76
RE_Poem <BUCYUM@gmail.com>
2026-05-20 22:35:52 +0800

    fix: each line is slower

--- Assets/Scripts/Notes/EachLineDrop.cs
+++ Assets/Scripts/Notes/EachLineDrop.cs
@@ -42,28 +42,30 @@ public class EachLineDrop : MonoBehaviour
         if (timing > 0) Destroy(gameObject);
         var distance = timing * speed + 4.8f;
         var destScale = distance * 0.4f + 0.51f;
-        var lineScale = Mathf.Abs(distance / 4.8f);
 
         var fakeTiming = timeProvider.FakeNoteTime - timeProvider.GetPositionAtTime(time);
         var fakeDistance = fakeTiming * speed + 4.8f;
         var fakeDestScale = fakeDistance * 0.4f + 0.51f;
-        var fakeLineScale = Mathf.Abs(fakeDistance / 4.8f);
 
         if (!UsingSV)
         {
             //fakeTiming = timing;
             fakeDistance = distance;
             fakeDestScale = destScale;
-            fakeLineScale = lineScale;
         }
 
 
+        transform.rotation = Quaternion.Euler(0, 0, -45f * (startPosition - 1));
+
+        if (fakeDestScale > 0.3f)
+            spriteRenderer.forceRenderingOff = false;
         if (fakeDistance < 1.225f)
         {
-            if (fakeDestScale > 0.3f) spriteRenderer.forceRenderingOff = false;
+            transform.localScale = new Vector3(1.225f / 4.8f, 1.225f / 4.8f, 1f);
+            return;
         }
 
-        transform.localScale = new Vector3(fakeLineScale, fakeLineScale, 1f);
-        transform.rotation = Quaternion.Euler(0, 0, -45f * (startPosition - 1));
+        var lineScale = Mathf.Abs(fakeDistance / 4.8f);
+        transform.localScale = new Vector3(lineScale, lineScale, 1f);
     }
 }
